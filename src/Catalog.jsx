73392d932dd85fe144495ee2ede15ccec6828550
Team-C22-039 <m266x0591@dicoding.org>
2023-01-31 11:09:50 +0700

    Change product name

--- src/Catalog.jsx
+++ src/Catalog.jsx
@@ -16,38 +16,38 @@ export default function Catalog() {
 			<div className="container mx-auto flex flex-wrap justify-evenly">
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 				<Cards
 					image="https://s3.amazonaws.com/www-inside-design/uploads/2020/10/aspect-ratios-blogpost-1x1-1.png"
-					name="Krupuk Babi"
-					description="Ini adalah krupuk babi sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
+					name="Krupuk Ayam"
+					description="Ini adalah krupuk Ayam sehat dan berkhasiat, bisa memperkuat tubuh Anda dalam jangka waktu 2 tahun"
 					whatsapp="+62877463923"
 				/>
 			</div>
