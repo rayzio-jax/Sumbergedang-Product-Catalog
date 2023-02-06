c4c19e770badb48f4bb440b603656baa49133b5e
Rayhan <rayhananthaakbar@gmail.com>
2023-02-06 19:55:44 +0700

    Change props name

--- src/components/ShinyButton.jsx
+++ src/components/ShinyButton.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./style/ShinyButton.scss";
 
 export default function ShinyButton(props) {
-	return <button className="btn-shiny">{props.btnValue}</button>;
+	return <button className="btn-shiny">{props.text}</button>;
 }
