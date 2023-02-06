e4c30bff4b4c93ec010d25563f78db1d4cc2f020
Rayhan <rayhananthaakbar@gmail.com>
2023-02-06 18:10:03 +0700

    Update Catalog component

--- src/Catalog.jsx
+++ src/Catalog.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { Carousel, CarouselItem } from "react-bootstrap";
 import Cards from "./components/Cards";
 import "./sass/Catalog.scss";
 
@@ -32,7 +33,13 @@ export default function Catalog() {
 			<div className="container mx-auto flex flex-wrap justify-evenly duration-300">
 				<Cards
 					btn_id="btn-order-1"
-					image={<img src="/product/product1.jpg" alt="products-card" />}
+					image={
+						<Carousel>
+							<Carousel.Item>
+								<img src="" alt="" />
+							</Carousel.Item>
+						</Carousel>
+					}
 					name="Cemal-cemil Bu Su"
 					description={
 						<p className="card-text">
@@ -45,7 +52,71 @@ export default function Catalog() {
 				/>
 				<Cards
 					btn_id="btn-order-2"
-					image={<img src="/product/product2.jpg" alt="products-card" />}
+					image={<img src="/product/product1.jpg" alt="products-card" />}
+					name="Jamu"
+					description={
+						<p className="card-text">
+							Sedia jamu sinom dan jamu beras kencur, dibuat dengan bahan alami
+							tanpa bahan pengawet. Tahan hingga 2-3 hari jika dimasukkan
+							kedalam lemari es.
+						</p>
+					}
+					whatsapp={number1}
+				/>
+				<Cards
+					btn_id="btn-order-3"
+					image={
+						<Carousel className="rounded-lg">
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/pdl-shoes.png"
+									alt="sepatu-pdl"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/pdh-boot.png"
+									alt="sepatu-boot-pdh"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/fantofel.png"
+									alt="sepatu-fantofel"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/safety-boot.png"
+									alt="sepatu-safety"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/school-shoes.png"
+									alt="sepatu-sekolah"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/docmart-female.png"
+									alt="sepatu-docmart-wanita"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/sandal-male.png"
+									alt="sandal-pria"
+								/>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									src="/product/sepatu-sandal/sandal-female.png"
+									alt="sandal-wanita"
+								/>
+							</Carousel.Item>
+						</Carousel>
+					}
 					name="Sepatu dan Sandal"
 					description={
 						<p className="card-text">
@@ -56,13 +127,60 @@ export default function Catalog() {
 					whatsapp={number1}
 				/>
 				<Cards
-					btn_id="btn-order-3"
+					btn_id="btn-order-4"
+					image={<img src="/product/product2.jpg" alt="products-card" />}
+					name="Krupuk Puli"
+					description={
+						<p className="card-text">
+							Dibuat dengan bahan alami, tahan hingga 3 bulan disuhu ruangan.
+						</p>
+					}
+					whatsapp={number1}
+				/>
+				<Cards
+					btn_id="btn-order-5"
 					image={<img src="/product/product3.jpg" alt="products-card" />}
 					name="Aneka Boneka"
 					description={
 						<p className="card-text">
 							Menyediakan berbagai macam jenis boneka dengan berbagai macam
-							ukuran
+							ukuran.
+						</p>
+					}
+					whatsapp={number1}
+				/>
+				<Cards
+					btn_id="btn-order-3"
+					image={<img src="/product/product3.jpg" alt="products-card" />}
+					name="Kripik Pisang Oleh-oleh Khas Jawa Timur"
+					description={
+						<p className="card-text">
+							Dibuat dengan bahan olahan pisang, bertahan hingga 3 bulan di suhu
+							ruangan. Menyediakan berbagai macam rasa dan juga kemasan.
+						</p>
+					}
+					whatsapp={number1}
+				/>
+				<Cards
+					btn_id="btn-order-3"
+					image={<img src="/product/product3.jpg" alt="products-card" />}
+					name="Kripik Pisang Raja Nangka"
+					description={
+						<p className="card-text">
+							Dibuat dengan bahan baku pisang, mempunyai tekstur renyah dan
+							gurih. Terdapat 2 varian rasa manis dan asin sedap.
+						</p>
+					}
+					whatsapp={number1}
+				/>
+				<Cards
+					btn_id="btn-order-3"
+					image={<img src="/product/product3.jpg" alt="products-card" />}
+					name="Madu Asli"
+					description={
+						<p className="card-text">
+							Ada berbagai macam ukuran dari 100 ml sampai dengan 460 ml. madu
+							ini juga memiliki berbagai manfaat untuk kesehatan.
 						</p>
 					}
 					whatsapp={number1}
