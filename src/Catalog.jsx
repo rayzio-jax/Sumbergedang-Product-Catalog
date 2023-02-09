115b0b3b3045a52ae9d1318e3300a3875b052adb
Rayhan <rayhananthaakbar@gmail.com>
2023-02-09 09:34:10 +0700

    Add Dropdown products

--- src/Catalog.jsx
+++ src/Catalog.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { Carousel } from "react-bootstrap";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 import Cards from "./components/Cards";
 import ShinyButton from "./components/ShinyButton";
+import $ from "jquery";
 import "./sass/Catalog.scss";
 
 export default function Catalog() {
 	const number1 = "6282334721859";
+	const title = "Pilih Produk";
+	const boneka = ["Boneka Panda", "Boneka Beruang", "Boneka Jerapah"];
 	return (
-		<div id="catalog">
+		<div id="catalog" className="catalog">
 			<Helmet>
 				<meta charSet="utf-8" />
 				<title>Katalog UMKM</title>
@@ -18,7 +23,7 @@ export default function Catalog() {
 					type="image/x-icon"
 				/>
 			</Helmet>
-			<div className="flex items-center">
+			<div className="flex mx-16 items-center">
 				<img
 					src="/catalog-icon.png"
 					className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] m-4"
@@ -27,7 +32,13 @@ export default function Catalog() {
 				<h1 className="flex-grow page-title font-poppins ml-[-5px] duration-300">
 					Katalog Produk UMKM
 				</h1>
-				<Link to={"/"} tabIndex={-1}>
+				<Link
+					to={"/"}
+					tabIndex={-1}
+					onClick={() => {
+						$(body).scrollTop(0);
+					}}
+				>
 					<ShinyButton className="btn-home" text="Homepage Desa" />
 				</Link>
 			</div>
@@ -49,7 +60,6 @@ export default function Catalog() {
 							pesanan di Hari Raya, Acara Hajatan dan Acara Penting lainnya.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-2"
@@ -57,12 +67,13 @@ export default function Catalog() {
 					name="Jamu"
 					description={
 						<p className="card-text">
-							Sedia jamu sinom dan jamu beras kencur, dibuat dengan bahan alami
-							tanpa bahan pengawet. Tahan hingga 2-3 hari jika dimasukkan
+							Tersedia berbagai jamu, sinom, dan jamu beras kencur, dibuat
+							dengan bahan alami tanpa bahan pengawet yang membuat produk ini
+							memiliki khasiat yang bermanfaat bagi tubuh. Produk ini
+							diperkirakan dapat bertahan hingga 2-3 hari jika dimasukkan
 							kedalam lemari es.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-3"
@@ -121,11 +132,12 @@ export default function Catalog() {
 					name="Sepatu dan Sandal"
 					description={
 						<p className="card-text">
-							Menyediakan berbagai macam sepatu dan sandal baik pria maupun
-							wanita, berbahan kulit sintetis.
+							Menyediakan berbagai macam sepatu dan sandal dengan bahan utama
+							yang digunakan dalam produk ini merupakan bahan kulit sintetis
+							yang bisa memberikan sensasi nyaman pada kaki Anda, tersedia untuk
+							pria dan wanita.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-4"
@@ -133,10 +145,13 @@ export default function Catalog() {
 					name="Krupuk Puli"
 					description={
 						<p className="card-text">
-							Dibuat dengan bahan alami, tahan hingga 3 bulan disuhu ruangan.
+							Krupuk berkualitas, renyah, dan gurih yang cocok sebagai makanan
+							sampingan. Dibuat dengan bahan alami sehingga krupuk ini dapat
+							dijadikan makanan pendamping yang aman bagi tubuh, produk ini
+							diperkirakan mampu bertahan sampai dengan 3 bulan dalam suhu
+							ruangan.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-5"
@@ -156,23 +171,32 @@ export default function Catalog() {
 					name="Aneka Boneka"
 					description={
 						<p className="card-text">
-							Menyediakan berbagai macam jenis boneka dengan berbagai macam
-							ukuran.
+							Tersedia berbagai jenis boneka dengan tekstur yang nyaman, dibuat
+							dengan desain yang lucu dan menawan sehingga memberikan kesan
+							mewah ketika diberikan kepada kekasih, kerabat, atau keluarga
+							Anda, tersedia juga berbagai ukuran boneka dari ukuran S sampai
+							dengan XL.
 						</p>
 					}
-					whatsapp={number1}
+					dropdown={
+						<DropdownButton title={title}>
+							<Dropdown.Item>{boneka[0]}</Dropdown.Item>
+							<Dropdown.Item>{boneka[1]}</Dropdown.Item>
+							<Dropdown.Item>{boneka[2]}</Dropdown.Item>
+						</DropdownButton>
+					}
 				/>
 				<Cards
 					btn_id="btn-order-3"
 					image={<img src="/product/product3.jpg" alt="products-card" />}
-					name="Kripik Pisang Oleh-oleh Khas Jawa Timur"
+					name="Aneka Oleh-oleh Khas Jawa Timur"
 					description={
 						<p className="card-text">
-							Dibuat dengan bahan olahan pisang, bertahan hingga 3 bulan di suhu
-							ruangan. Menyediakan berbagai macam rasa dan juga kemasan.
+							Menjual berbagai macam kripik dan krupuk, seperti krupuk kentang
+							udang pedas dan kripik pisang, rasanya yang lezat dan renyah. Juga
+							dipasarkan di tokoh oleh-oleh khas Jawa Timur.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-3"
@@ -180,11 +204,12 @@ export default function Catalog() {
 					name="Kripik Pisang Raja Nangka"
 					description={
 						<p className="card-text">
-							Dibuat dengan bahan baku pisang, mempunyai tekstur renyah dan
-							gurih. Terdapat 2 varian rasa manis dan asin sedap.
+							Olahan pisang yang menjadi sebuah kripik renyah, gurih, dan kaya
+							akan rasa, sehingga cocok untuk menjadi cemilan Anda saat sedang
+							sibuk atau sedang bersantai. Memiliki 2 varian rasa, asin sedap
+							dan manis.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 				<Cards
 					btn_id="btn-order-3"
@@ -192,11 +217,13 @@ export default function Catalog() {
 					name="Madu Asli"
 					description={
 						<p className="card-text">
-							Ada berbagai macam ukuran dari 100 ml sampai dengan 460 ml. madu
-							ini juga memiliki berbagai manfaat untuk kesehatan.
+							Olahan madu asli tanpa ada campuran bahan pengawet dan gula
+							sintetis yang bisa memberikan khasiat penuh dan rasa kaya akan
+							madu murni yang bisa memuaskan dan menyehatkan Anda. Tersedia
+							dalam berbagai macam ukuran, mulai dari 100 ml sampai dengan 460
+							ml.
 						</p>
 					}
-					whatsapp={number1}
 				/>
 			</div>
 		</div>
